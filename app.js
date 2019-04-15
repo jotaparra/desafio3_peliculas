9ea178c0af5668a143881e8d987e46e5b5d52a39
Jennifer Parra <jparra.biblio@gmail.com>
2019-04-15 17:15:41 -0400

    Initial al src commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('Estamos conectados')
 
-const movies =['psycho', 'Citizen%20Kane', 'Gone%20with%20the%20Wind', 'casablanca', 'spartacus', 'La%20grande%20illusion', 'Modern%20Times', 'Singin%27%20in%20the%20Rain', 'metropolis', ];
+const movies =['psycho', 'Citizen%20Kane', 'Gone%20with%20the%20Wind', 'casablanca', 'rebecca', 'La%20grande%20illusion', 'Modern%20Times', 'Singin%27%20in%20the%20Rain', 'metropolis', 'Forbidden%20Planet','2001:A%20Space%20Odyssey', 'seven%20samurai', 'Vertigo', 'Some%20Like%20It%20Hot','Rebel%20Without%20a%20Cause','ordet', 'm'];
 
 function classicMovie () {movies.map(function(item) {
     fetch('http://www.omdbapi.com/?apikey=5688f11a&t='+ item)
@@ -8,16 +8,16 @@ function classicMovie () {movies.map(function(item) {
       .then(data => {
         document.getElementById('root').innerHTML+=
       `<div class="row">
-      <div class="principal-box col-8 offset-2">
+      <div class="principal-box col-12 col-lg-8 offset-lg-2">
           <div class="titulo col-12">
               <div class="row align-items-center">
-                  <div class="col-3 col-miniatura">
+                  <div class="col-2">
                       <div class="circle">
                           <img src=${data.Poster} alt="miniatura poster película">
                       </div>
                   </div>
-                  <div class="col-9">
-                      <p class="parrafo pl-2">
+                  <div class="col-10">
+                      <p class="movie-title pl-5 pl-md-3">
                           ${data.Title}
                       </p>
                   </div>
